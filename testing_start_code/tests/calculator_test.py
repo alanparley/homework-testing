a6d660d9456c495e520aa3ebe9b0a388800e1935
Alan Parley <alanparley@hotmail.com>
2020-09-15 20:16:48 +0100

    added all tests and all run aok

--- testing_start_code/tests/calculator_test.py
+++ testing_start_code/tests/calculator_test.py
@@ -5,3 +5,12 @@ from src.calculator import add, divide, multiply, subtract
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(5, add(2, 3))
+
+    def test_subtract(self):
+        self.assertEqual(3, subtract(10, 7))
+
+    def test_divide(self):
+        self.assertEqual(7, divide(21, 3))
+
+    def test_multiply(self):
+        self.assertEqual(50, multiply(10, 5))
